d53b8fec9415183c8232354ae28baa371bde1401
Reece D'Souza <rebads25@gmail.com>
2025-05-03 10:02:04 +0530

    Updates

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 
